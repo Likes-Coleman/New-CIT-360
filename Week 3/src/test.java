06265a272b709b1ec115a2eb1ee836fbc09199c1
Coleman_Likes <crlikes@byui.edu>
2020-01-13 16:48:53 -0700

    new java

---
+++ Week 3/src/test.java
@@ -0,0 +1,18 @@
+/**
+ * This is Week 1.1: Using Listing 1.2 in the textbook as an example, write a
+ * Java Program that displays your name, CIT260 and your section, your home
+ * town, and your favorite dessert. Name the class in your program W1dot1. Your
+ * program should conform to the style guidelines outlined in this weeks
+ * PowerPoint slides.
+ */
+public class w1dot1 {
+
+    public static void main(String[] argv) {
+
+        System.out.println("Coleman Likes");
+        System.out.println("CIT260-017");
+        System.out.println("Draper, Utah");
+        System.out.println("Banana Bread");
+        
+    }
+}
